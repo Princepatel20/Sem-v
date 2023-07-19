91a03ecaa1f8b360ec490898e0c0947be899b1d6
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-07-19 23:01:04 +0530

    Create insert.py

---
+++ DAA/insert.py
@@ -0,0 +1,38 @@
+import time
+dict = {'1': 'D', '2': 'W', '3': 'A', '4': 'S', '5': 'E', '6': 'U', '7': 'G'}
+def ins(dict):
+    items = list(dict.items())
+    n = len(items)
+
+    for i in range(1, n):
+        key = items[i]
+        j = i - 1
+        while j >= 0 and items[j][1] > key[1]:
+            items[j + 1] = items[j]
+            j -= 1
+
+        items[j + 1] = key
+    sorted = {k: v for k, v in items}
+    return sorted
+
+start = time.time()
+sorted_dictionary = ins(dict)
+end = time.time()
+print("sorted element by using insertion sort is:",sorted_dictionary)
+print("Time taken by the average case is:",end-start,"sec")
+
+
+sorted_items = sorted(dict.items(), key=lambda x: x[1])
+dict1 = {k: v for k, v in sorted_items}
+start1 = time.time()
+sorted_dictionary1 = ins(dict1)
+end1 = time.time()
+print("sorted element by using insertion sort is:",sorted_dictionary1)
+print("Time taken by the best case is:",end1-start1,"sec")
+
+dict2 = {k: v for k, v in sorted(dict.items(), key=lambda x: x[1], reverse=True)}
+start2 = time.time()
+sorted_dictionary2 = ins(dict2)
+end2 = time.time()
+print("sorted element by using insertion sort is:",sorted_dictionary2)
+print("Time taken by the worst case is:",end2-start2,"sec")
