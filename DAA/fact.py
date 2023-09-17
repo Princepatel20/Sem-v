ab4d8ce18aa16b69a5a89fc03880a2a7fd70f13a
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-09-17 16:38:02 +0530

    Update fact.py

--- DAA/fact.py
+++ DAA/fact.py
@@ -23,3 +23,32 @@ print("Factorial using iterative method",iterative_factorial(num))
 print("Time taken by the iterative function is: ",t2)
 print("Factorial using recursive method is : ", recursive_factorial(num))
 print("Time taken by the recursiv function is: ",t3)
+
+#Method to computer FIB ( iterative )
+Import time
+S=time.time()
+def fact(n):
+    result = 1
+    for i in range(1,n+1):
+        result*=i
+        print(result)
+    E=time.time()
+    a=int(input("Enter a number: "))
+    print("Factorial is: ")
+    fact(a)
+    print("Time for Iterative is: ")
+    print(E-S*60*60)
+
+#Method to computer FIB ( Recursive )
+s=time.time()
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+    e=time.time()
+    n = int(input("Enter a number: "))
+    print("Factorial is: ")
+    print(factorial(n))
+    print("Time for recursive is: ")
+    
