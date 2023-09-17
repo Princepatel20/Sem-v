5048e88db9838db3c26a3fa7c2aa46a5be7439bc
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-09-17 16:54:03 +0530

    Update fib.py

--- DAA/fib.py
+++ DAA/fib.py
@@ -35,3 +35,31 @@ it(num)
 print("Fibonacii using  recursive",fib(num))
 print("Time taken by the itrative method is : ",end1-start1)
 print("Time taken by the recursive method is : ",end-start)
+
+
+#Method to compute FIB ( iterative )
+import time
+start=time.time()
+def fibonacci(n):
+	a=0
+	b=1
+	for i in range(n+1):
+		print(a, end = "")
+		a,b =,a+b
+	end=time.time()
+	n=int(input("Enter the number of terms: "))
+	fibonacci(n)
+	print("Time For Itertive: ")
+	print(end-start*60*60)
+
+#Method to compute FIB (Recursion )
+start1=time.time()
+def fibonacci(n):
+	if n <=1:
+		return n
+	return fibonacci(n-1) + fibonacci(n-2)
+end1=time.time()
+n= int(input("Enter a number: "))
+print(fibonacci(n))
+print("Time for REcursion: ")
+print(end1-start1*60*60)
