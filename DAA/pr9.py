512fbd12d61dcbbbee7b4ceebc9af90f51d7a951
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-09 09:15:52 +0530

    Create pr9.py

---
+++ DAA/pr9.py
@@ -0,0 +1,28 @@
+# Define graph as adjacency list
+graph = {
+  'a': ['b', 'c'],
+  'b': ['d', 'e'],
+  'c': ['f'],
+  'd': [],
+  'e': ['f'],
+  'f': []
+}
+
+# Function for breadth-first search
+def bfs(graph, start, end):
+  visited = []
+  queue = [start]
+
+  while queue:
+    current = queue.pop(0) 
+    if current not in visited:
+      visited.append(current)
+      if current == end:
+        print(visited)
+        return
+      neighbors = graph[current]
+      for neighbor in neighbors:
+        queue.append(neighbor)
+
+# Call BFS function  
+bfs(graph, 'a', 'f')
