2e9190e71e33bbd9d962cd6e2733170b450cb303
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-09 09:15:24 +0530

    Create pr8.py

---
+++ DAA/pr8.py
@@ -0,0 +1,37 @@
+# Define the ingredients
+baking_soda = 0.5 
+pancake_mix = 1
+oil = 1
+milk = 0.75
+
+# Heat the griddle
+def heat_griddle():
+  print("Heating griddle...")
+
+# Mix all the ingredients  
+def mix_ingredients(baking_soda, pancake_mix, oil, milk):
+  print("Mixing ingredients...")
+
+# Spoon the mix onto a hot griddle
+def spoon_mix():
+  print("Spooning mix onto griddle...")
+  
+# Wait for the pancakes to start bubbling
+def wait_for_bubbles():
+  print("Waiting for bubbles...")
+  
+# Turn the pancakes over and cook until golden brown  
+def cook_until_golden_brown():
+  print("Cooking pancakes until golden brown...")
+
+# Heat up some syrup
+def heat_syrup():
+  print("Heating up syrup...")
+  
+# Call the functions in order
+heat_griddle()
+mix_ingredients(baking_soda, pancake_mix, oil, milk)  
+spoon_mix()
+wait_for_bubbles()
+cook_until_golden_brown()
+heat_syrup()
