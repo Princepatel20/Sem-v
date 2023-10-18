8d5952e2ca3fd3f077009be8442e7543ae9f59d5
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-18 20:12:15 +0530

    Create Pr5b.php

---
+++ Web development/PHP/Pr5b.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+</head>
+<body>
+  <h1>Login</h1>
+  <form action="process_login.php" method="POST">
+    <label for="username">Username (Email):</label>
+    <input type="email" id="username" name="username" required><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
