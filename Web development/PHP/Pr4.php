933f2dd73aa87b867440b6595aa32db4c122cbc1
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-18 19:30:21 +0530

    Update Pr4.php

--- Web development/PHP/Pr4.php
+++ Web development/PHP/Pr4.php
@@ -1,35 +1,19 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-
-<title>Simple Interest Calculator</title>
-
+    <title>Simple Interest Calculator</title>
 </head>
-
 <body>
-
-<h1>Simple Interest Calculator</h1>
-
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-
-<label for="loanAmount">Loan Amount:</label>
-
-<input type="number" name="loanAmount" required><br>
-
-<label for="interestRate">Rate of Interest (%):</label>
-
-<input type="number" name="interestRate" required><br>
-
-<label for="years">Number of Years:</label>
-
-<input type="number" name="years" required><br>
-
-<input type="submit" value="Calculate">
-
-</form>
-
+    <h1>Simple Interest Calculator</h1>
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <label for="loanAmount">Loan Amount:</label>
+        <input type="number" name="loanAmount" required><br>
+        <label for="interestRate">Rate of Interest (%):</label>
+        <input type="number" name="interestRate" required><br>
+        <label for="years">Number of Years:</label>
+        <input type="number" name="years" required><br>
+        <input type="submit" value="Calculate">
+    </form>
 <?php
 
 function calculateSimpleInterest($loanAmount, $interestRate, $years) {
@@ -37,38 +21,23 @@ function calculateSimpleInterest($loanAmount, $interestRate, $years) {
 $simpleInterest = ($loanAmount * $interestRate * $years) / 100;
 
 return $simpleInterest;
-
 }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$loanAmount = $_POST["loanAmount"];
-
-$interestRate = $_POST["interestRate"];
-
-$years = $_POST["years"];
-
+    $loanAmount = $_POST["loanAmount"];
+    $interestRate = $_POST["interestRate"];
+    $years = $_POST["years"];
 if (!empty($loanAmount) && !empty($interestRate) && !empty($years)) {
-
-$simpleInterest = calculateSimpleInterest($loanAmount, $interestRate, $years);
-
-echo "Loan Amount: $" . $loanAmount . "<br>";
-
-echo "Rate of Interest: " . $interestRate . "%<br>";
-
-echo "Number of Years: " . $years . "<br>";
-
-echo "Simple Interest: $" . $simpleInterest;
-
-} else {
-
-echo "Please fill in all fields.";
+    $simpleInterest = calculateSimpleInterest($loanAmount, $interestRate, $years);
+    echo "Loan Amount: $" . $loanAmount . "<br>";
+    echo "Rate of Interest: " . $interestRate . "%<br>";
+    echo "Number of Years: " . $years . "<br>";
+    echo "Simple Interest: $" . $simpleInterest;
     }
-
+else {
+    echo "Please fill in all fields.";
+    }
 }
-
 ?>
-
 </body>
-
 </html>
