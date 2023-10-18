963d5dd3ba6cbc8c98a695200eacc2df20e70fba
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-18 20:46:05 +0530

    Update Pr5a.php

--- Web development/PHP/Pr5a.php
+++ Web development/PHP/Pr5a.php
@@ -1,3 +1,4 @@
+index.html
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,5 +28,25 @@
   <input type="tel" id="contactNumber" name="contactNumber" required><br><br>
   <input type="submit" value="Register">
  </form>
+</body>
+</html>
+
+login.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+</head>
+<body>
+  <h1>Login</h1>
+  <form action="process_login.php" method="POST">
+    <label for="username">Username (Email):</label>
+    <input type="email" id="username" name="username" required><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    <input type="submit" value="Login">
+  </form>
 </body>
 </html>
