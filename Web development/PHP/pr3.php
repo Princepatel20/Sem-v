04f7025116a7941434d1bf0ed4006f153c9a0d7f
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-18 19:22:50 +0530

    Update pr3.php

--- Web development/PHP/pr3.php
+++ Web development/PHP/pr3.php
@@ -1,24 +1,13 @@
 <?php
-
 class Dakota {
-
 public function add($n1, $n2) {
-
 return $n1 + $n2;}
-
 public function sub($n1, $n2) {
-
 return $n1 - $n2;}}
-
 $DakotaObj = new Dakota();
-
 $result = $DakotaObj->add(10, 30);
-
 $result_sub = $DakotaObj->sub(60, 10);
 echo "The sum is: " . $result;
-
 echo "<br>";
-
 echo "The sub is: ".$result_sub;
-
 ?>
