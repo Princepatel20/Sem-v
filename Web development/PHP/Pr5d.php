de018b9098d7dbe4be39d096f6b6f83d3b2934a7
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-19 08:12:23 +0530

    Update Pr5d.php

--- Web development/PHP/Pr5d.php
+++ Web development/PHP/Pr5d.php
@@ -1,72 +1,37 @@
 <?php
-
 $servername = "localhost";
-
 $username = "root";
-
 $password = "";
-
 $dbname = "employee";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
-
 die("Connection failed: " . $conn->connect_error);}
-
 if (isset($_GET['id'])) {
-
 $employeeId = $_GET['id'];
-
 $sql = "SELECT * FROM MyGuests WHERE id = $employeeId";
-
 $result = $conn->query($sql);
-
 if ($result->num_rows == 1)
-
 {
-
 $row = $result->fetch_assoc();
-
 echo "<h3>Update Employee Details</h3>";
-
 echo "<form method='post' action='update_employee.php?id=$employeeId'>";
   echo "Employee Name: <input type='text' name='employee_name'
-
 value='{$row['emp_name']}'><br>";
-
 echo "Birthdate: <input type='date' name='birthdate'
-
 value='{$row['emp_birthdate']}'><br>";
-
 echo "<input type='submit' name='update' value='Update'>";
-
 echo "</form>";
-
 if (isset($_POST['update'])) {
-
 // Update the employee details in the database
-
 $newName = $_POST['employee_name'];
-
 $newBirthdate = $_POST['birthdate'];
-
 $updateSql = "UPDATE MyGuests SET emp_name = '$newName', emp_birthdate =
-
 '$newBirthdate' WHERE id = $employeeId";
-
 if ($conn->query($updateSql) === TRUE) {
-
 echo "Record updated successfully.";
-
 } else {
-
 echo "Error updating record: " . $conn->error;
-
 } }
-
 } else {
-
 echo "Employee not found or multiple records found with the same ID."; }}
-
 ?>
