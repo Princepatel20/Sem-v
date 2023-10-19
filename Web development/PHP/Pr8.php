8c8b8591b728a5640a88470f3a78dd220e1025a6
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-19 08:02:28 +0530

    Create Pr8.php

---
+++ Web development/PHP/Pr8.php
@@ -0,0 +1,97 @@
+<?php
+
+session_start();
+
+// Generate a random reset token (6 characters)
+
+function generateResetToken() {
+
+ return 
+
+substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
+
+WXYZ'), 0, 6);
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['reset_email'])) {
+
+ $reset_email = $_POST['reset_email'];
+
+ $reset_token = generateResetToken();
+
+ // Store the reset token in the session (for demonstration purposes)
+
+ $_SESSION['reset_token'] = $reset_token;
+
+ // Send the reset token to the user's email (in a real application, you would send an email here)
+
+ echo "A reset token has been sent to your email: $reset_token";
+
+}
+
+if (isset($_POST['reset_password'])) {
+
+ $entered_token = $_POST['reset_token'];
+  $new_password = $_POST['new_password'];
+
+ // Validate the reset token (compare with the stored token in the session)
+
+ if ($_SESSION['reset_token'] === $entered_token) {
+
+ echo "Password reset successful. New password: $new_password";
+
+ // Update the user's password in the database (in a real application)
+
+ } else {
+
+ echo "Invalid reset token. Please try again.";
+ }
+}
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+ <meta charset="UTF-8">
+
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+ <title>Password Reset</title>
+
+</head>
+
+<body>
+
+ <h1>Request Password Reset</h1>
+
+ <form action="" method="POST">
+
+ <label for="reset_email">Enter your email:</label>
+
+ <input type="email" id="reset_email" name="reset_email" required><br><br>
+
+ <input type="submit" value="Reset Password">
+
+ </form>
+<h2>Reset Password</h2>
+
+ <form action="" method="POST">
+
+ <label for="reset_token">Enter the reset token from your email:</label>
+
+ <input type="text" id="reset_token" name="reset_token" required><br><br>
+
+ <label for="new_password">Enter a new password:</label>
+
+ <input type="password" id="new_password" name="new_password" required><br><br>
+
+ <input type="submit" name="reset_password" value="Reset Password">
+
+ </form>
+
+</body>
+
+</html>
