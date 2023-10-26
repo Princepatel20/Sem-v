35de46877bd5d3b5adf9f2565160419cd9ca7037
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2023-10-26 21:30:16 +0530

    Create Pr8.py

---
+++ DAA/Pr8.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+def topological_sort(graph):
+ visited = set()
+ stack = []
+ def visit(node):
+ if node not in visited:
+ visited.add(node)
+ for neighbor in graph[node]:
+ visit(neighbor)
+ stack.append(node)
+ for node in list(graph.keys()):
+ visit(node)
+ return stack[::-1]
+graph = defaultdict(list)
+graph["heat griddle"].append("mix ingredients")
+graph["mix ingredients"].append("spoon mix onto griddle")
+graph["mix ingredients"].append("heat up syrup")
+graph["spoon mix onto griddle"].append("flip pancakes")
+graph["flip pancakes"].append("cook until golden brown on the bottom")
+order_of_steps = topological_sort(graph)
+for step in order_of_steps:
+ print(step)
